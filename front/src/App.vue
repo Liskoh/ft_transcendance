c66caf8901b9b9ac353d2676e2005384eca57231
Liskoh <wydvek@gmail.com>
2023-02-28 00:10:20 +0100

    feat: (hjordan): working on big command refactoring

--- front/src/App.vue
+++ front/src/App.vue
@@ -26,6 +26,54 @@ const parseCommand = (channelId: number, command: string) => {
     return;
   }
 
+  abstract class AbstractCommand{
+    public readonly prefix: string;
+    public readonly key: string;
+
+    protected constructor(prefix: string, key: string) {
+      this.prefix = prefix;
+      this.key = key;
+    }
+
+    public emitCommand(object: Object): void {
+      SOCKET_SERVER.emit(this.key, object);
+    }
+
+    public abstract getCommandData(channelId: number, commandArgs: string[]): Object;
+  }
+
+  class InviteCommand extends AbstractCommand {
+
+    constructor(prefix: string, key: string) {
+      super(prefix, key);
+    }
+
+    public getCommandData(channelId: number, commandArgs: string[]): Object {
+      return {
+        channelId: channelId,
+        nickname: commandArgs[0],
+      };
+    }
+  }
+
+  const commands: AbstractCommand[] = [
+    new InviteCommand('/invite', 'inviteUser'),
+  ];
+
+  function getCommandToSend() : AbstractCommand {
+    for (let i = 0; i < commands.length; i++) {
+      if (commands[i].prefix.toLowerCase() === commandName.toLowerCase()) {
+        return commands[i];
+      }
+    }
+  }
+
+  const commandToSend = getCommandToSend();
+
+  if (commandToSend) {
+    commandToSend.emitCommand(commandToSend.getCommandData(channelId, commandArgs));
+  }
+
   switch (commandName) {
     case `/${Command.INVITE}`:
       SOCKET_SERVER.emit('inviteUser',
