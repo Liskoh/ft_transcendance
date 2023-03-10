7b7eac4dd314fb1d4cd27f327328635e4ab2ce6a
Hugo JORDAN <wydvek@gmail.com>
2023-03-10 23:43:30 +0100

    feat: (hjordan): reworking chat frontend

--- front/src/App.vue
+++ front/src/App.vue
@@ -126,11 +126,11 @@ a {
 }
 
 .c-page {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  /*width: 100%;*/
+  /*height: 100%;*/
+  /*display: flex;*/
+  /*flex-direction: column;*/
+  /*justify-content: space-between;*/
 }
 
 .c-subpage {
